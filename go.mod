211b129f211133ee17fc4b9dee14e9a945852dce
Matthias Petermann <mp@d2ux.net>
2025-07-23 13:03:29 +0200

    Refactor

--- go.mod
+++ go.mod
@@ -1,23 +1,17 @@
-module focalsort
+module d2ux.net/focalsort
 
 go 1.23
 
 require (
-	github.com/gdamore/tcell/v2 v2.8.1
-	github.com/rivo/tview v0.0.0-20241227133733-17b7edb88c57
 	github.com/rwcarlsen/goexif v0.0.0-20190401172101-9e8deecbddbd
+	github.com/sigurn/crc8 v0.0.0-20220107193325-2243fe600f9f
 	github.com/sirupsen/logrus v1.9.3
-	github.com/spf13/cobra v1.8.1
+	github.com/tidwall/gjson v1.18.0
 	golang.org/x/sys v0.29.0
 )
 
 require (
-	github.com/gdamore/encoding v1.0.1 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.16 // indirect
-	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/term v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,9 +1,90 @@
+// File: main.go
 package main
 
 import (
-    "focalsort/cmd"
+	"fmt"
+	"github.com/sigurn/crc8"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"d2ux.net/focalsort/internal/config"
+	"d2ux.net/focalsort/internal/exif"
+	"d2ux.net/focalsort/internal/hash"
+	_ "d2ux.net/focalsort/internal/imaging"
+	"d2ux.net/focalsort/internal/util"
+	"github.com/sirupsen/logrus"
 )
 
+// init sets up logging.
+// This is run automatically before main().
+func init() {
+	logrus.SetFormatter(&logrus.TextFormatter{FullTimestamp: true})
+	logrus.SetLevel(logrus.DebugLevel)
+}
+
+// main is the CLI entry point.
+// Sets up command-line flags, environment variable bindings, and delegates to the engine.
 func main() {
-    cmd.Execute()
+
+	config.InitFromFlags()
+	cfg := config.GetConfig()
+
+	processImages(cfg.SourceDirectory)
+}
+
+func processImages(importFolder string) {
+
+	var processedFiles = 0
+	var totalFiles int
+
+	files, err := os.ReadDir(importFolder)
+	if err != nil {
+		logrus.Fatal("Error reading directory:", err)
+	}
+
+	totalFiles = len(files)
+
+	err = filepath.Walk(importFolder, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() && strings.ToLower(filepath.Ext(info.Name())) == ".jpg" {
+			logMsg := "Processing file: " + info.Name()
+			logrus.Info(logMsg)
+
+			dateTime, camera, err := exif.ExtractExifData(path)
+			if err != nil {
+				logrus.Error("Error extracting EXIF date: " + err.Error())
+				return nil
+			}
+
+			table := crc8.MakeTable(crc8.CRC8)
+			cameraCrc := crc8.Checksum([]byte(camera), table)
+			camera = fmt.Sprintf("%02X", cameraCrc)
+
+			checksum, err := hash.CalculateChecksum(path)
+			if err != nil {
+				logrus.Error("Error calculating checksum: " + err.Error())
+				return nil
+			}
+
+			//			sharpness := imaging.EvaluateImageQuality(path)
+
+			err = utils.RenameImage(path, config.GetConfig().TargetDirectory, dateTime, camera, checksum)
+			if err != nil {
+				logrus.Error("Error renaming: " + err.Error())
+				return nil
+			}
+
+			processedFiles++
+			logrus.Infof("Progress: %d/%d", processedFiles, totalFiles)
+		}
+		return nil
+	})
+
+	if err != nil {
+		logrus.Fatalf("Error processing import folder: %v", err)
+	}
 }
